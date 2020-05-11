d418774696bae6bf93df8751e0422414f0d985e3
sarath <osarath265@gmail.com>
2020-05-11 19:15:26 +0530

    random number logic is added

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const app = require('express')();
 
 app.get('/',(req,res)=>{
-    res.end("Hello World");
+    res.end("working");
+});
+
+app.get('/get-value',(req,res)=>{
+    let val = Math.floor(Math.random() * 90 + 10);
+    res.json({value:val});
+    res.end()
 });
 
 app.listen(process.env.PORT || 3000,()=>{console.log("server up on 3000")});
